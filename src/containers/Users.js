1703630ac220d11137712ff85583e77f1c98d3e5
Rakesh <rakesh@s2bnexgens-MacBook-Pro.local>
2020-05-23 22:18:08 +0530

    Webpack basic setup from scratch for React App

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The users</h1>
+        <p>Hello, User</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
